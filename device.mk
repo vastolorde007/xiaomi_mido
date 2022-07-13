09b7e9fcdf4ab775a91b19f1a91ead71ff263c9c
shubhojitnag <shubhojitnag2009@gmail.com>
2022-07-13 13:03:53 +0530

    Revert "mido: Replace Snap with GrapheneCamera"
    This reverts commit 00a40d87b03a44e02055a391c020b8ebb6c278b2.

--- device.mk
+++ device.mk
@@ -161,9 +161,6 @@ PRODUCT_PACKAGES += \
     libmm-qcamera \
     libui_shim
 
-PRODUCT_PACKAGES += \
-    GrapheneCamera
-
 # Cgroup and task_profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
