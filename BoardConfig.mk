4caabec500c145f36f1354753dcd2a7ad16fd6b8
shubhojitnag <shubhojitnag2009@gmail.com>
2023-01-08 10:34:47 +0530

    mido : Fixed denials and added ignore neverallows flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,6 +161,7 @@ VENDOR_SECURITY_PATCH := 2022-09-05
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
